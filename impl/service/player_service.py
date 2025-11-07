3518865968503450f602c8b5445ac7a88bedb5cb
Finn Eyer <finn.eyer24@gmail.com>
2025-11-07 15:48:55 +0100

    [TASK] Prepared player_service to use the Monte Carlo Treesearch model with ML trump selection

--- impl/service/player_service.py
+++ impl/service/player_service.py
@@ -1,12 +1,12 @@
 import logging
 from jass.service.player_service_app import PlayerServiceApp
-from impl.agents.agent_monte_carlo import MCTSAgent, MCTSConfig
-
+from impl.agents.agent_mc_ml import MCTSAgent_ML, MCTSConfig
 
 logging.basicConfig(level=logging.DEBUG)
 
 app = PlayerServiceApp('player_service')
 config = MCTSConfig(iterations=500, time_limit_ms=250, determinization_samples=8)
-app.add_player('mcts1', MCTSAgent(config))
-app.add_player('mcts2', MCTSAgent(config))
+
+
+app.add_player('mcts_ml', MCTSAgent_ML(config))
 
