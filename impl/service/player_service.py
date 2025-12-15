73fb0f91373106663854b5a82f32ea33d24bf507
Finn Eyer <finn.eyer24@gmail.com>
2025-12-15 13:31:14 +0100

    [TASK] Configured second player andpoint to compare performances of parameter values

--- impl/service/player_service.py
+++ impl/service/player_service.py
@@ -5,8 +5,11 @@ from impl.agents.agent_mc_ml import MCTSAgent_ML, MCTSConfig
 logging.basicConfig(level=logging.DEBUG)
 
 app = PlayerServiceApp('player_service')
-config = MCTSConfig(iterations=1000, time_limit_ms=1000)
+
+config_1 = MCTSConfig(iterations=1000, time_limit_ms=1000)
+config_2 = MCTSConfig(iterations=500, time_limit_ms=250)
 
 
-app.add_player('mcts_ml', MCTSAgent_ML(config))
+app.add_player('mcts_ml_final', MCTSAgent_ML(config_1))
+app.add_player('mcts_ml_midterm', MCTSAgent_ML(config_2))
 
