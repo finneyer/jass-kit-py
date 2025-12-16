935ed76147552fddbe6468b6151899ccf7e24608
Finn Eyer <finn.eyer24@gmail.com>
2025-12-16 18:46:40 +0100

    [TASK] Added MCTS DNN bot to player service

--- impl/service/player_service.py
+++ impl/service/player_service.py
@@ -1,6 +1,7 @@
 import logging
 from jass.service.player_service_app import PlayerServiceApp
 from impl.agents.agent_mc_ml import MCTSAgent_ML, MCTSConfig
+from impl.agents.agent_mc_dnn import MCTSAgent_DNN
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -12,4 +13,4 @@ config_2 = MCTSConfig(iterations=500, time_limit_ms=250)
 
 app.add_player('mcts_ml_final', MCTSAgent_ML(config_1))
 app.add_player('mcts_ml_midterm', MCTSAgent_ML(config_2))
-
+app.add_player('mcts_dnn', MCTSAgent_DNN(config_1))
